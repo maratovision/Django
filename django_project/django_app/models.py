ea577a9a1a5aa88fb52ae735327b661aaded285f
Maratovision <maratovision@gmail.com>
2021-01-15 21:08:58 +0600

    new

--- django_project/django_app/models.py
+++ django_project/django_app/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class News(models.Model):
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+    date_create = models.DateTimeField(auto_now_add=True)
